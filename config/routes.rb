5d19241b2bf746c2589e12aef01ec659904e09d5
cbottone <cbottone@apple.com>
2012-06-03 15:42:39 -0700

    Adding styling and improving routes
    Signed-off-by: cbottone <cbottone@apple.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+
+ # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root to: 'static_pages#home'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -59,4 +64,5 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
