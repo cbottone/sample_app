49b9026d06a568973c7cd628f50e430f6b0a99e2
cbottone <cbottone@apple.com>
2012-06-03 16:41:36 -0700

    Finishes layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
 
  # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root to: 'static_pages#home'
+  
+  match '/signup',  to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
